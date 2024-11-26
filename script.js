a75a22fecc8705bfcda4c3bd17df97913da984fa
danceq <140273750+danceqqq@users.noreply.github.com>
2024-11-26 12:36:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const heroes = {
     },
     "Джонсон": {
         "counterPick": "Минотавр",
-        "img": "johnson.jpg"
+        "img": "johnson.png"
     },
     "Минотавр": {
         "counterPick": "Джонсон",
@@ -20,37 +20,14 @@ const heroes = {
 };
 
 // Обработка выбора героя
-const myTeamSelect = document.getElementById('my-team-select');
-const enemyTeamSelect = document.getElementById('enemy-team-select');
-const counterPickName = document.getElementById('counter-pick-name');
+const heroImgs = document.querySelectorAll('.hero-select img');
+const counterPickImg = document.getElementById('counter-pick-img');
 
-// Добавление героев в селекты
-Object.keys(heroes).forEach(hero => {
-    const option = document.createElement('option');
-    option.value = hero;
-    option.text = hero;
-    myTeamSelect.appendChild(option);
-    enemyTeamSelect.appendChild(option);
-});
-
-// Установка значений для селекта "Моя команда"
-myTeamSelect.value = Object.keys(heroes)[0];
-
-// Обработка выбора героя
-myTeamSelect.addEventListener('change', () => {
-    const selectedHero = myTeamSelect.value;
-    const counterPick = heroes[selectedHero].counterPick;
-    counterPickName.textContent = counterPick;
-    counterPickName.classList.remove('selected');
-});
-
-enemyTeamSelect.addEventListener('change', () => {
-    const selectedHero = enemyTeamSelect.value;
-    const counterPick = heroes[selectedHero].counterPick;
-    counterPickName.textContent = counterPick;
-    if (myTeamSelect.value === counterPick) {
-        counterPickName.classList.add('selected');
-    } else {
-        counterPickName.classList.remove('selected');
-    }
+heroImgs.forEach((img, index) => {
+    img.src = heroes[Object.keys(heroes)[index]].img;
+    img.addEventListener('click', () => {
+        const selectedHero = Object.keys(heroes)[index];
+        const counterPick = heroes[selectedHero].counterPick;
+        counterPickImg.src = heroes[counterPick].img;
+    });
 });
